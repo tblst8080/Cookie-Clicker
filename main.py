0f92f5a5a26a0c700f76a67a52b995296633b18c
Germs & Spices <123396762+tblst8080@users.noreply.github.com>
2023-03-02 19:41:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ from selenium.webdriver.chrome.options import Options
 
 
 def cookie_count():
+    """Count the number of cookies"""
     count = driver.find_element(By.ID, "cookies")
     count = count.text.split(" ")[0].replace(",","")
     return int(count)
 
 def highest_upgrade():
+    """Find the highest upgrade available and select it"""
     enabled_options = driver.find_elements(By.CLASS_NAME, "unlocked")
     print([option.find_element(By.CLASS_NAME, "productName").text for option in enabled_options])
     choice = enabled_options[-1]
@@ -22,20 +24,28 @@ def highest_upgrade():
     time_mark = time.perf_counter()
     return time_mark
 
+
+# Modify webdriver to remain open
 options = Options()
 options.add_experimental_option('detach', True)
 
+# URL
 url='https://orteil.dashnet.org/cookieclicker/'
 
+
+# Set up web driver
 web_driver = ChromeDriverManager().install()
 service = Service(web_driver)
 driver = webdriver.Chrome(service=service, options=options)
 
+# Access game site
 driver.get(url)
 time.sleep(5)
 english_button = driver.find_element(By.ID, "langSelect-EN")
 english_button.click()
 time.sleep(5)
+
+# Click
 cookie = driver.find_element(By.CSS_SELECTOR, "button#bigCookie")
 time_mark = time.perf_counter()
 while True:
